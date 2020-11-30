d9459ef1bcee882adcb8aa69faef52f7cb6a5605
Muedgar <edgarmutangana@gmail.com>
2020-11-30 21:03:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,738 @@
+/*
+sometimes I want to do something that is not bad but I know that what is good for me is to do another thing.
+for example, when I know that I should sit study and work hard, but I get an urge to go and have fun and relax
+today I resisted the urge and now I am trying to write code. Yesterday I did not resist the urge and I didn't 
+write code, let me say that I had a plan of building things and solving exercises daily. Let me explain my pro-
+blem very well. I have a goal of becoming a world class software engineer, specifically having done 25 million
+exercises in the fields of operating systems development, computer security systems development, computer networks
+development, cyber security systems development and application softwares development, the exercises will be many
+because of the 20 million exercises that I want to invest in sciences, and I have to build 100k software projects
+. Now the second goal I have is to achieve these goals by 2024, which means there is a must learning pace that I 
+must adopt every daily, NOT DOING THIS LEARNING PACE SETS ME UP FOR MORE HARD WORK IN THE FUTURE AND DOING MORE
+REDUCES THE WORK I HAVE TO DO IN THE FUTURE AND TIME, WHAT I DO DAILY AFFECTS BY TIME OR WORK ETHIC.
+
+I become sad because the work ethic I have to master to achieve this goal is hard and almost impossible which means
+that since my personality is a weak one, I am not even able to sit for long hours and just do what I can let alone 
+what I have to do, I give up fast.
+by
+
+quotes of daily improvement, measured according to the daily inputs only
+
+quotes of pace consistency, measured by comparing the daily inputs and pace expected
+
+
+I need some kind of reminder that it will end I just have to stick on the plan.
+I need some kind of goal related consequences app that shows me how my daily actions affect my entire
+goal by having more work to do per day, or working for more years, which will have some kind of daily
+evaluation, and once I fill in the daily evaluation, I get the consequences or the newly calculated
+remaining work, the daily pace it would take, the time it would take once I obey the daily pace, if I 
+increased efforts depending on my history, if I am on the right pace percentage. 
+
+what is grit? the ability of doing something when someone's life depends on it?
+
+so when I think about what I have to go through and that my failure will affect someone's life or even
+mine in a very bad way, I become afraid, and I try to create the illusion that life will not be affected
+this fear forces someone to get rid of it, how do one get rid of fear, by proving to himself that 
+not doing anything will be okay, and this leads to negligance, so fear can be a good thing because
+it tells you the truth, but I hate fear? Isn't there any way to get what you want and still not have
+fear, like is there a way of being aware of danger and deal with it with out being afraid.
+
+How to build a goal tracking app that promotes awareness of problems instead of fear of problems?
+
+how do people promote fear?
+the number one people who promote fear are not the ones who have it, but the ones who believe that,
+for progress to exist, one has to be afraid?
+
+But before I solve the problem, let me first simulate the problem in the most realistic way as possible
+at the extent that any one using the app will immediately understand what I am talking about, in fact
+this is what I can achieve with a computer and a programming language, explaining to a computer my,
+problems and figuring out the solutions together, because just because I have whatsapp doesn't mean that 
+I don't talk to friends no more, it is just that not the computer helps alot. And that is why in this case
+the computer will also help me to solve the problems I have in my life, which is fear and how I can achieve
+my goals, with less fear.
+
+This is what happens,
+agent,
+////////////////////////////
+
+///////////////////////////
+circumstances,
+goals,
+actions, // 
+results, // this comes usually at the end of the day, because it is when all work should have been done, and it is when I reflect
+consequences, // this is in every stage because even before I was born, it was known that it will be easy or hard for me to learn
+
+reminder, and hours tracker, along a timeline that I should have finished the 
+first app or the first phase of development
+ 
+*/
+/*
+WHAT IS ANOTHER THING THAT IS WRONG ABOUT THIS PROBLEM? 
+*/
+/*
+let ds_1 = {
+    goal_exercises: null,
+    goal_projects: null,
+    goal_date_how_many_years_from_now: null,
+    start_date: null,
+    end_date: null,
+    possible_exercises_pace_daily: null,
+    possible_projects_pace_daily: null,
+    required_exercises_pace_daily: null,
+    required_projects_pace_daily: null,
+    possibility: undefined,
+    impossibility: undefined,
+    impossible_by_how_long: undefined
+}
+let ds_2 = {
+    dates_worked_on: ["date_first",...,"date_last"] // length not exceeding days of specified.
+    date_first: {
+        exercises_done: null,
+        projects_done: null,
+        required_exercises_pace_daily: null,
+        required_projects_pace_daily: null,
+        possibility: undefined,
+        impossibility: undefined,
+        impossibility_by_how_long: undefined
+    }
+    ...
+    date_last: {
+        exercises_done: null,
+        projects_done: null,
+        required_exercises_pace_daily: null,
+        required_projects_pace_daily: null,
+        possibility: undefined,
+        impossibility: undefined,
+        impossibility_by_how_long: undefined
+    }
+}
+let ds_3 = {
+    quotes: {
+        quote_1: "",
+        ...
+        quote_100: ""
+    },
+    quote_category: {
+        daily_improvement: null,
+        pace_consistency: null,
+        quotes: undefined
+    }
+}
+let ds_4 = {
+    hours_day_to_work: [["start_time","end_time"],...,["start_time","end_time"]],
+    tasks_possible_per_day: null,
+    time_possible_per_task: null,
+    expected_schedule_possible: {
+        time_first: [{task_status: null, time_taken: null, time_interval_done: null}, ...,{task_status: null, time_taken: null, time_interval_done: null}]
+        ...
+        time_last: [{task_status: null, time_taken: null, time_interval_done: null}, ...,{task_status: null, time_taken: null, time_interval_done: null}]
+    },
+    real_day_schedule_done: {
+        time_first: [{task_status: null, time_taken: null, time_interval_done: null}, ...,{task_status: null, time_taken: null, time_interval_done: null}]
+        ...
+        time_last: [{task_status: null, time_taken: null, time_interval_done: null}, ...,{task_status: null, time_taken: null, time_interval_done: null}]
+    }
+}
+let ds_5 = {
+    time_not_working: [["start_time","end_time"],...,["start_time","end_time"]],
+
+}
+let ds_6 = {
+
+}
+let ds_7 = {
+
+}
+let ds_8 = {
+
+}
+let ds_9 = {
+
+}
+let ds_10 = {
+
+}
+let ds_11 = {
+
+}
+let ds_12 = {
+
+}
+let ds_13 = {
+
+}
+let ds_14 = {
+
+}
+let ds_15 = {
+
+}
+let ds_16 = {
+
+}
+let ds_17 = {
+
+}
+let ds_18 = {
+
+}
+let ds_19 = {
+
+}
+let ds_20 = {
+
+}
+*/
+
+
+
+/////// start part 1
+// start storage
+let ds_1 = {
+    goal_exercises: null,
+    goal_projects: null,
+    goal_date_how_many_years_from_now: null,
+    start_date: null,
+    end_date: null,
+    possible_exercises_pace_daily: null,
+    possible_projects_pace_daily: null,
+    required_exercises_pace_daily: null,
+    required_projects_pace_daily: null,
+    possibility: undefined,
+    impossibility: undefined,
+    impossible_by_how_long: undefined
+}
+// end storage
+// start input
+function ds_1_input() {
+    // create temporary variables
+    let g_e = null;
+    let g_p = null;
+    let g_d_h_m_y_f_n = null;
+    let s_d = null;
+    let e_d = null;
+    let p_e_p_d = null;
+    let p_p_p_d = null;
+    let r_e_p_d = null;
+    let r_p_p_d = null;
+    let p = undefined;
+    let imp = undefined;
+    let imp_b_h_l = undefined;
+    // create a random function to populate temporary variables
+    function populateGoalExercises(goalExer) {
+        // getting exercises goals
+        let input = goalExer;
+        if((typeof(input)=="number")&&(input>1000)) {
+            g_e = input;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    function populateGoalProjects(goalProj) {
+        // getting exercises goals
+        let input = goalProj;
+        if((typeof(input)=="number")&&(input>12)) {
+            g_p = input;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    function populateDeadlineInYears(goalDead) {
+        // getting exercises goals
+        let input = goalDead;
+        if((typeof(input)=="number")&&(input>0)) {
+            g_d_h_m_y_f_n = input;
+            return true;
+        }else {
+            return false;
+        }
+    }
+    function populateDates(startDate, endDate) {
+        // getting inputs
+        s_d = startDate;
+        e_d = endDate;
+    }
+    function populatePossibleExercises(possibleExercises) {
+        let input = possibleExercises;
+        p_e_p_d = input;
+    }
+    function populatePossibleProjects(possibleProjects) {
+        let input = possibleProjects;
+        p_p_p_d = input;
+    }
+    function populateExpectedExercises() {
+        r_e_p_d = Math.floor((g_e)/(g_d_h_m_y_f_n * 365));
+    }
+    function populateExpectedProjects() {
+        r_p_p_d = Math.floor((g_p)/(g_d_h_m_y_f_n * 365));
+    }
+    function populatePossibility() {
+        if((r_e_p_d<p_e_p_d)||(r_p_p_d<p_p_p_d)) {
+            p = true;
+        }else {
+            p = false;
+        }
+    }
+    function populateImpossibility() {
+        if((r_e_p_d>p_e_p_d)||(r_p_p_d>p_p_p_d)) {
+            imp = true;
+        }else {
+            imp = false;
+        }
+    }
+    function populateImpossibilityByHowFar() {
+        if(imp == true) {
+            var imp_e = null;
+            var imp_p = null;
+            imp_e = (g_d_h_m_y_f_n * 365)/(p_e_p_d);
+            imp_p = (g_d_h_m_y_f_n * 365)/(p_p_p_d);
+            imp_b_h_l = {val1: imp_e, val2: imp_p};
+        }else {
+            return false;
+        }
+    }
+    // inputing by calling these functions
+    ///
+    ///
+    ///
+    
+    // call populator
+    function populator() {
+        let random_goalExer;
+        function randGE() {
+            let array = [120000, 120930, 140000,110000,100500];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_goalExer = array[newElement];
+        }
+        randGE();
+        let random_goalProj;
+        function randGP() {
+            let array = [186, 798, 830,390,492];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_goalProj = array[newElement];
+        }
+        randGP();
+        let random_goalDead;
+        function randDead() {
+            let array = [10, 7, 5,8,9];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_goalDead = array[newElement];
+        }
+        randDead();
+        let random_startDate;
+        function randStart() {
+            let array = ["December 23 2020", "January 23 2021", "March 23 2021","June 23 2021","November 23 2021"];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_startDate = array[newElement];
+        }
+        randStart();
+        let random_endDate;
+        function randEnd() {
+            let array = ["December 23 2025", "January 23 2024", "March 23 2027","June 23 2029","November 23 2026"];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_endDate = array[newElement];
+        }
+        randEnd();
+        let random_possibleExercises;
+        function randPossibleExer() {
+            let array = [200, 400, 500,600,800];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_possibleExercises = array[newElement];
+        }
+        randPossibleExer();
+        let random_possibleProjects;
+        function randPossibleProj() {
+            let array = [20, 40, 50,60,80];
+            let newElement = Math.floor(Math.random() * array.length);
+            random_possibleProjects = array[newElement];
+        }
+        randPossibleProj();
+        // after generating random values call functions
+        populateGoalExercises(random_goalExer);
+        populateGoalProjects(random_goalProj);
+        populateDeadlineInYears(random_goalDead);
+        populateDates(random_startDate, random_endDate);
+        populatePossibleExercises(random_possibleExercises);
+        populatePossibleProjects(random_possibleProjects);
+        populateExpectedExercises();
+        populateExpectedProjects();
+        populatePossibility();
+        populateImpossibility();
+        populateImpossibilityByHowFar();
+    }
+    populator();
+    // populate permanent storage
+    ds_1.goal_exercises =  g_e;
+    ds_1.goal_projects = g_p;
+    ds_1.goal_date_how_many_years_from_now = g_d_h_m_y_f_n;
+    ds_1.start_date = s_d;
+    ds_1.end_date = e_d;
+    ds_1.possible_exercises_pace_daily = p_e_p_d;
+    ds_1.possible_projects_pace_daily = p_p_p_d;
+    ds_1.required_exercises_pace_daily = r_e_p_d;
+    ds_1.required_projects_pace_daily = r_p_p_d;
+    ds_1.possibility = p;
+    ds_1.impossibility = imp;
+    ds_1.impossible_by_how_long = imp_b_h_l;
+
+    console.log(ds_1);
+    return true;
+}
+// test input
+ds_1_input();
+// end input
+// start output
+// let ds_2 = {
+//     dates_worked_on: [], // length not exceeding days of specified.
+//     date_first: { // keys are the dates worked on
+//         exercises_done: null,
+//         projects_done: null,
+//         required_exercises_pace_daily: null,
+//         required_projects_pace_daily: null,
+//         possibility: undefined,
+//         impossibility: undefined,
+//         impossibility_by_how_long: undefined
+//     }
+// }
+let ds_2 = {
+    dates_worked_on : []
+}
+// inputs
+function recordDay(date,exercisesD, projectsD) {
+    function ds2PopulateRequiredExercisesPace() {
+        return ds_1.required_exercises_pace_daily;
+    }
+    function ds2PopulateRequiredProjectsPace() {
+        return ds_1.required_projects_pace_daily;
+    }
+    function ds2Possibility(exer,proj) {
+        if((exer<ds_1.required_exercises_pace_daily)&&(proj<ds_1.required_projects_pace_daily)) {
+            return "false";
+        }else {
+            return "true";
+        }
+    }
+    function ds2Impossibility(exer,proj) {
+        if((exer>ds_1.required_exercises_pace_daily)||(proj>ds_1.required_projects_pace_daily)) {
+            return "false";
+        }else {
+            return "true";
+        }
+    }
+    if((ds_2.dates_worked_on.length)>(ds_1.populateDeadlineInYears*365)) {
+        console.log("deadline reached...");
+        return false;
+    }else {
+        ds_2.dates_worked_on.push(date);
+        ds_2[date] = {
+            exercises_done: exercisesD,
+            projects_done: projectsD,
+            required_exercises_pace_daily: ds2PopulateRequiredExercisesPace(),
+            required_projects_pace_daily: ds2PopulateRequiredProjectsPace(),
+            possibility: ds2Possibility(exercisesD,projectsD),
+            impossibility: ds2Impossibility(exercisesD,projectsD)
+        };
+    }
+}
+
+// random outputs
+function randomOutputs() {
+    let inputs = [{date: "November 23 2021", exercises: 30, projects: 10},{date: "October 21 2021", exercises: 33, projects: 11},{date: "January 1 2021", exercises: 40, projects: 15},{date: "November 3 2021", exercises: 60, projects: 90},{date: "December 23 2021", exercises: 30, projects: 10}];
+    let newElement = Math.floor(Math.random() * inputs.length);
+    recordDay(inputs[newElement].date,inputs[newElement].exercises,inputs[newElement].projects);
+}
+for(let i=0;i<100;i++) {
+    randomOutputs();
+}
+// end output
+console.log(ds_2);
+/////// end part 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
